ba75e58b63491582544ae7212225b5dc5fdeeea3
aneeshb123plr <aneeshb123plr@gmail.com>
2020-03-29 13:50:24 +0530

    Setup prod deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +18,13 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-app.get("/", (req, res) => res.json({ msg: "Welcome to Contact Keeper Api" }));
+// Serve static file in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
